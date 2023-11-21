5b1d67e425f45309444c6e81de0fc49a28219cf6
Janisroz <janisrozkalejs@gmail.com>
2023-11-21 19:30:28 +0000

    Create models in models.py file

--- knowledgebase/models.py
+++ knowledgebase/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    vid_url = models.URLField(max_length=200)
+    description = models.CharField(max_length=300)
+    thumbnail = models.URLField(max_length=200)
+    upload_date = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
+    keywords = models.ManyToManyField(User, related_name='keyword', blank=True)
+
+    class Meta:
+        ordering = ['-upload_date']
+
+    def __str__(self):
+        return self.title
+
+    def total_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=300)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
+
+
+class Keyword(models.Model):
+    keyword = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.keyword
