33a5fa69dae288f43aebbda72bd4f34a23f67b4c
Janisroz <janisrozkalejs@gmail.com>
2023-11-23 20:43:14 +0000

    Create views for index.html

--- knowledgebase/views.py
+++ knowledgebase/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models impor Post 
 
-# Create your views here.
+class VideoList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-upload_date')
+    template_name = 'index.html'
+    paginate_by = 8
+
+
