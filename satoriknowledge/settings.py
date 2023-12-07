1c34c29524e44927736470b87cb75126424cd4f1
Janisroz <janisrozkalejs@gmail.com>
2023-12-07 14:03:59 +0000

    Add allauth to site and style

--- satoriknowledge/settings.py
+++ satoriknowledge/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-janisroz-satoriknowledge-pi79cwesmn.us2.codeanyapp.com',
+ALLOWED_HOSTS = ['8000-janisroz-satoriknowledge-slot6r9v15.us2.codeanyapp.com',
                  'satori-knowledgebase-f34b4f67eb41.herokuapp.com',
                  '8001-janisroz-satoriknowledge-pi79cwesmn.us2.codeanyapp.com']
 
