61c0e40f8e9ed40fd58f63ceb53a1afe7347c6f9
Janisroz <janisrozkalejs@gmail.com>
2023-12-14 20:54:32 +0000

    Test deployment commit

--- satoriknowledge/settings.py
+++ satoriknowledge/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-janisroz-satoriknowledge-slot6r9v15.us2.codeanyapp.com',
                  'satori-knowledgebase-f34b4f67eb41.herokuapp.com',
