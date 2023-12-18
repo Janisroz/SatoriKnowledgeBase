23f23adfdb1ba5cf5aecb02f22cdacd385e55441
Janisroz <janisrozkalejs@gmail.com>
2023-12-18 21:37:55 +0000

    Add dynamic debug setting

--- satoriknowledge/settings.py
+++ satoriknowledge/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-janisroz-satoriknowledge-slot6r9v15.us2.codeanyapp.com',
                  'satori-knowledgebase-f34b4f67eb41.herokuapp.com',
